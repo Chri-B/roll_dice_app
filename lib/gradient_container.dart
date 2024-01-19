acfb3832480651bd788a7b4dd0e7e14a6767ed44
Christian Beretta <beretta@efarmgroup.com>
2024-01-19 11:42:20 +0100

    custom widget

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+// Custom class / widget
+import 'package:flutter/material.dart';
+import 'package:prject/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 96, 28, 190)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(child: StyledText('Hello World!')),
+    );
+  }
+}
+
